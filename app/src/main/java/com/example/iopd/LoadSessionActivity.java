edf19371e63403c5ab73f46706615e7d1a268f1a
Putthichai Chaithummapakorn <44458971+putthichai@users.noreply.github.com>
2019-01-22 15:29:53 +0700

    remove title bar

--- app/src/main/java/com/example/iopd/LoadSessionActivity.java
+++ app/src/main/java/com/example/iopd/LoadSessionActivity.java
@@ -3,13 +3,15 @@ package com.example.iopd;
 import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.Window;
 
 public class LoadSessionActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.activity_loadsession);
 
         Intent intent = new Intent(LoadSessionActivity.this,MainMenuActivity.class);
         startActivity(intent);
